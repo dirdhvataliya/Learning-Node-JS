c4ad3d7a41d3c6572f05593a606b91eb4841ec9b
Dirdh Vataliya <dirdhvataliya2003@gmail.com>
2025-03-20 15:53:13 +0530

    loading html files with fs module

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
-import http from 'http'
+import http from 'http';
+import fs from 'fs/promises';
+import url  from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
 
-const server = http.createServer((req,res)=>{
+// GET CURRENT PATH
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer(async (req,res)=>{
     try{
         if(req.method==='GET'){
+            let filePath;
             if(req.url === '/'){
-                res.writeHead(200,{ 'Content-Type':'text/html' });
-                res.end('<h1>Homepage</h1>')
+                filePath = path.join(__dirname,'public','index.html')
             }
             else if(req.url==='/about'){
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.end('<h1>About</h1>')
+                filePath = path.join(__dirname,'public','about.html')
             }
             else{
-                res.writeHead(404,{'Content-Type':'text/html'});
-                res.end('<h1>Error! Page not found</h1>')
+                throw new Error('not found')
             }
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type','text/html');
+            res.write(data);
+            res.end();
+
         }
         else{
             throw new Error('method not allowed') 
