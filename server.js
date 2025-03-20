1e4cbbbf46cd35a4b5077c9a646717ee2879ba49
Dirdh Vataliya <dirdhvataliya2003@gmail.com>
2025-03-20 14:52:05 +0530

    routing by node js

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 import http from 'http'
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req,res)=>{
-    // res.setHeader('Content-Type','text/html')
-    // res.statusCode = 200;
-    res.writeHead(500,{ 'Content-Type':'application/json' })
-    res.end(JSON.stringify({messege:'server error'}));
+    try{
+        if(req.method==='GET'){
+            if(req.url === '/'){
+                res.writeHead(200,{ 'Content-Type':'text/html' });
+                res.end('<h1>Homepage</h1>')
+            }
+            else if(req.url==='/about'){
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.end('<h1>About</h1>')
+            }
+            else{
+                res.writeHead(404,{'Content-Type':'text/html'});
+                res.end('<h1>Error! Page not found</h1>')
+            }
+        }
+        else{
+            throw new Error('method not allowed') 
+        }
+    }
+    catch(error){
+        res.writeHead(500,{'Content-Type':'text/plain'});
+        res.end('Error! Page not found')
+    }
+    
 });
 
 server.listen(PORT,()=>{
